1f00b32fdcd3dde5a111fcc965f701daff294929
Andreas <glanshammar70@gmail.com>
2026-04-24 15:25:20 +0200

    Added SavedStateHandle to item list.

--- Yggdrasil.Mobile/composeApp/src/commonMain/kotlin/core/yggdrasil/viewmodels/ItemListViewModel.kt
+++ Yggdrasil.Mobile/composeApp/src/commonMain/kotlin/core/yggdrasil/viewmodels/ItemListViewModel.kt
@@ -1,21 +1,28 @@
 package core.yggdrasil.viewmodels
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 data class Item(val id: Int, val title: String, val content: String)
 
-class ItemListViewModel : ViewModel() {
-    private val _items = MutableStateFlow(emptyList<Item>())
+class ItemListViewModel(
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+    private val _items = MutableStateFlow(
+        savedStateHandle.get<List<Item>>("items") ?: emptyList()
+    )
     val items = _items.asStateFlow()
 
     fun addItem(title: String, content: String) {
         val newItem = Item(_items.value.size + 1, title, content)
         _items.value += newItem
+        savedStateHandle["items"] = _items.value
     }
 
     fun deleteItem(itemId: Int) {
         _items.value = _items.value.filter { it.id != itemId }
+        savedStateHandle["items"] = _items.value
     }
 }
