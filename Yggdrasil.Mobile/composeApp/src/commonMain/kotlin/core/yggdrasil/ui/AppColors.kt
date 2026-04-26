2d661eb66a2b782911a25995cec7aade54de021c
Andreas <glanshammar70@gmail.com>
2026-04-26 18:28:37 +0200

    Added colors & dimensions

---
+++ Yggdrasil.Mobile/composeApp/src/commonMain/kotlin/core/yggdrasil/ui/AppColors.kt
@@ -0,0 +1,16 @@
+package core.yggdrasil.ui
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val Primary = Color(0xFF1E88E5)
+    val Secondary = Color(0xFF26A69A)
+    val Background = Color(0xFFF5F5F5)
+    val Surface = Color(0xFFFFFFFF)
+    val Error = Color(0xFFB00020)
+    val OnPrimary = Color(0xFFFFFFFF)
+    val OnSecondary = Color(0xFF000000)
+    val OnBackground = Color(0xFF000000)
+    val OnSurface = Color(0xFF000000)
+    val OnError = Color(0xFFFFFFFF)
+}
---
+++ Yggdrasil.Mobile/composeApp/src/commonMain/kotlin/core/yggdrasil/ui/Dimensions.kt
@@ -0,0 +1,18 @@
+package core.yggdrasil.ui
+
+import androidx.compose.ui.unit.dp
+
+object Dimensions {
+    val SpacingSmall = 8.dp
+    val SpacingMedium = 16.dp
+    val SpacingLarge = 24.dp
+    val SpacingExtraLarge = 32.dp
+
+    val IconSizeSmall = 24.dp
+    val IconSizeMedium = 48.dp
+    val IconSizeLarge = 64.dp
+    val IconSizeExtraLarge = 120.dp
+
+    val ButtonSize = 64.dp
+    val ListItemImageSize = 56.dp
+}
