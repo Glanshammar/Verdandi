c325bd22f1e1cd75286f424975d4f4c83ae47b87
Andreas <glanshammar70@gmail.com>
2026-04-27 18:01:34 +0200

    Added item repo

---
+++ Yggdrasil.Mobile/composeApp/src/commonMain/kotlin/core/yggdrasil/data/ItemRepository.kt
@@ -0,0 +1,24 @@
+package core.yggdrasil.data
+
+import core.yggdrasil.viewmodels.Item
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+object ItemRepository {
+    private val _items = MutableStateFlow<List<Item>>(emptyList())
+    val items = _items.asStateFlow()
+
+    fun addItem(title: String, content: String) {
+        val newItem = Item(_items.value.size + 1, title, content)
+        _items.update { it + newItem }
+    }
+
+    fun deleteItem(itemId: Int) {
+        _items.update { currentItems -> currentItems.filter { it.id != itemId } }
+    }
+    
+    fun clear() {
+        _items.value = emptyList()
+    }
+}
