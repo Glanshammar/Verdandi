05d438fdd831647aeb5fabd3b16d07616350d6ab
Andreas <glanshammar70@gmail.com>
2026-04-27 15:47:50 +0200

    Added HTTP Client

---
+++ Yggdrasil.Mobile/composeApp/src/commonMain/kotlin/core/yggdrasil/network/NetworkClient.kt
@@ -0,0 +1,18 @@
+package core.yggdrasil.network
+
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+object NetworkClient {
+    val client = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            })
+        }
+    }
+}
